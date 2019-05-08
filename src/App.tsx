1b0a321e3f34d37dd341fd9bcf0fafacce1f1eb5
zb <2479107942@qq.com>
2019-05-08 20:00:28 +0800

    fisrt submit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }
+}
+
+export default App;
